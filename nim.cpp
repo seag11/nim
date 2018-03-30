a230c8a674874149812e61f3a4f92739772978ae
seag11 <seag11@users.noreply.github.com>
2018-03-29 22:03:38 -0700

    main file

---
+++ nim.cpp
@@ -0,0 +1,15 @@
+/****************
+* game of nim
+*****************/
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+	cout << "game of nim" << endl;	
+	
+	return 0;
+}
